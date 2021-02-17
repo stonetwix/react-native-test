e446b18309f7e6f17e7d18d33a1a03853842b1c6
Moa Stenqvist <moa@stonetwig.se>
2021-02-17 14:48:47 +0100

    clean up code.

--- components/index.js
+++ components/index.js
@@ -4,43 +4,41 @@ import StyledButton from './StyledButton';
 import styles from './styles';
 
 const DestinationItem = (props) => {
+const { name, tagline, taglineCTA, image } = props.destination;
 
-  const { name, tagline, taglineCTA, image } = props.destination;
+  return (
+    <View style={styles.destinationContainer}>
 
-    return (
-        <View style={styles.destinationContainer}>
+      <ImageBackground source={image} 
+      style={styles.image}
+      />
 
-        <ImageBackground source={image} 
-        style={styles.image}
-        />
-
-        <View style={styles.titles}>
-          <Text style={styles.title}>{name}</Text>
-          <Text style={styles.subtitle}>
-            {tagline}
-            {' '}
-            <Text style={styles.subtitleCTA}>
-              {taglineCTA}
-            </Text>
+      <View style={styles.titles}>
+        <Text style={styles.title}>{name}</Text>
+        <Text style={styles.subtitle}>
+          {tagline}
+          {' '}
+          <Text style={styles.subtitleCTA}>
+            {taglineCTA}
           </Text>
-        </View> 
+        </Text>
+      </View> 
 
-        <View style={styles.buttonsContainer}>
-          <StyledButton type='primary' 
-            content={'Book now'} 
+      <View style={styles.buttonsContainer}>
+        <StyledButton type='primary' 
+          content={'Book now'} 
+          onPress={() => {
+            console.warn('Book now was pressed')
+          }}/>
+
+          <StyledButton type='secondary' 
+            content={'More details'} 
             onPress={() => {
-              console.warn('Book now was pressed')
-            }}/>
-
-            <StyledButton type='secondary' 
-              content={'More details'} 
-              onPress={() => {
               console.warn('More details was pressed')
-            }}/>
-        </View>
-        
-      </View>
-    );
+          }}/>
+      </View>        
+    </View>
+  );
 };
 
 export default DestinationItem;
